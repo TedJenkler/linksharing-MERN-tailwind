9b9d5becc3f5da0fe8631efc6fc4b6e1bf891936
TedJenkler <Tedjenkler@gmail.com>
2024-05-27 06:46:35 +0200

    getLinkByEmail route added

--- server/routes/linkRoutes.js
+++ server/routes/linkRoutes.js
@@ -4,7 +4,30 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 const Links = require('../schema/linkSchema');
+const User = require('../schema/userSchema');
 const secretKey = process.env.JWT_SECRET_KEY;
+
+router.get('/getByEmail/:email', async (req, res) => {
+    try {
+        const userEmail = req.params.email;
+        const user = await User.findOne({ email: userEmail });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const userLinks = await Links.find({ userId: user._id })
+
+        if (!userLinks || userLinks.length === 0) {
+            return res.status(404).json({ message: 'No links found for this user' });
+        }
+
+        res.status(200).json(userLinks)
+    }catch (error) {
+        console.error('Error fetching user links:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
 
 router.get('/getAll', async (req, res) => {
     try {
